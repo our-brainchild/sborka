764ea3da2daaf55b8aa7bb4a7982cbe770a13719
Пользователь <>
2016-08-13 12:12:10 +0300

    modified in footer block in ADMIN

--- admin/pages/blocks/footer.php
+++ admin/pages/blocks/footer.php
@@ -4,8 +4,8 @@
     <div class="row color-text">
       <div class="col-md-3 col-lg-3">
         <p>© 2011 — 2016 sborka.ru<br>
-        Условия использования<br>
-        Cделано в "Yolka Corporation"</p>
+        Условия использования<br></p>
+        <!-- Cделано в "Yolka Corporation"</p> -->
       </div>
       <div class="col-md-9 col-lg-9">
         <p>Понедельник - Пятница с 9:00 до 19:00, Суббота с 9:30 до 16:00<br>
